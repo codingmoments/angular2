caa4e0a6ad204061fa063d73200a9ece3d9f4d24
Nilesh Wani <n.wani@maxxton.com>
2025-04-28 12:24:14 +0530

    Learning Angular from Udemy

---
+++ 01-starting-project/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.componenet';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from "./tasks/tasks.component";
+
+import { DUMMY_USERS } from './dummy-users';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent, TasksComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+
+
+  onSelectUser(id: string) {
+    console.log('selected user with id ' + id);
+  }
+}
