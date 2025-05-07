3c7d575e63500078761e1113e8274aa65107d9e9
Nilesh Wani <nileshwaani@gmail.com>
2025-05-07 12:15:01 +0530

    Learning Angular from Udemy

--- 02-investment-calculator/src/app/app.component.ts
+++ 02-investment-calculator/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { type InvestmentInput } from './investment-input.model';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,36 @@ import { UserInputComponent } from './user-input/user-input.component';
   templateUrl: './app.component.html',
   imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  annualData = [{}];
+
+  onInputSubmit(investmentInput: InvestmentInput) {
+    this.annualData = this.calculateInvestmentResults(investmentInput);
+  }
+
+  private calculateInvestmentResults(investmentInput: InvestmentInput) {
+    const annualData = [];
+    const { initialInvestment, annualInvestment, expectedReturn, duration } =
+      investmentInput;
+
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+    console.log(annualData);
+    return annualData;
+  }
+}
--- 02-investment-calculator/src/app/user-input/user-input.component.ts
+++ 02-investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+import { type InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,17 +11,19 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  enteredInitialInvestment?: number;
-  enteredAnnualInvestment?: number;
-  enteredExpectedReturn?: number;
-  enteredDuration?: number;
+  enteredInitialInvestment = 0;
+  enteredAnnualInvestment = 0;
+  enteredExpectedReturn = 0;
+  enteredDuration = 0;
+
+  @Output() inputSubmit = new EventEmitter<InvestmentInput>();
 
   onSubmit() {
-    console.log(
-      this.enteredInitialInvestment,
-      this.enteredAnnualInvestment,
-      this.enteredExpectedReturn,
-      this.enteredDuration
-    );
+    this.inputSubmit.emit({
+      initialInvestment: this.enteredInitialInvestment,
+      annualInvestment: this.enteredAnnualInvestment,
+      expectedReturn: this.enteredExpectedReturn,
+      duration: this.enteredDuration,
+    });
   }
 }
