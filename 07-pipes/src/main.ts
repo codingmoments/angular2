dfce5a35df18630f56d4247cd9e3641e9c38588e
Nilesh Wani <nileshwaani@gmail.com>
2025-05-21 11:04:55 +0530

    Learning Angular from Udemy

--- 07-pipes/src/main.ts
+++ 07-pipes/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import localeUs from '@angular/common/locales/es-US';
+import localeDe from '@angular/common/locales/de';
 
-bootstrapApplication(AppComponent)
-  .catch((err) => console.error(err));
+registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeUs, 'us');
+registerLocaleData(localeDe, 'de');
+
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
