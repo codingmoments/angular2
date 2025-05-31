8fa862adf58cf71dbba5a86ebde6058c5f046339
Nilesh Wani <nileshwaani@gmail.com>
2025-05-31 16:10:11 +0530

    Learning Angular from Udemy

--- 10-observables/src/app/app.component.ts
+++ 10-observables/src/app/app.component.ts
@@ -8,7 +8,7 @@ import {
 } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,10 +20,28 @@ export class AppComponent implements OnInit {
   clickCount$ = toObservable(this.clickCount);
   interval$ = interval(1000);
   intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      if (timesExecuted > 5) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value ...');
+      subscriber.next({ message: 'new value' });
+      timesExecuted++;
+    }, 2000);
+  });
 
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('completed!')
+    });
+
     const subscription = this.clickCount$.subscribe({
       next: (val) => console.log(`Clicked button ${val} times.`),
     });
